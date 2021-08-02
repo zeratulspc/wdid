40999f7483b7cd4e8d3fba1d27655bf54883be25
zeratulspc <zeratulspc@gmail.com>
2021-08-03 00:14:58 +0900

    style widgets

--- lib/page/auth/authPage.dart
+++ lib/page/auth/authPage.dart
@@ -10,8 +10,9 @@ class AuthPage extends StatefulWidget {
 
 class _AuthPageState extends State<AuthPage> {
 
-  bool isLogin = true;
+  final GlobalKey authWidgetKey = GlobalKey();
 
+  bool isLogin = true;
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +21,6 @@ class _AuthPageState extends State<AuthPage> {
       body: Column(
         children: [
           Expanded(
-            flex: 1,
             child: Center(
               child: Container(
                 child: Text(
@@ -35,17 +35,22 @@ class _AuthPageState extends State<AuthPage> {
             ),
           ),
           AnimatedContainer(
-            duration: Duration(milliseconds: 500),
+            curve: Curves.easeIn,
+            duration: Duration(milliseconds: 100),
             width: Get.width,
             padding: EdgeInsets.all(30),
             decoration:BoxDecoration(
               color: Color(0xffffffff),
               borderRadius: BorderRadius.vertical(top: Radius.circular(30))
             ),
-            child: isLogin ? LoginWidget(()=>setState(()=>isLogin=false)) : RegisterWidget(()=>setState(()=>isLogin=true)),
+            child: Container(
+              key: authWidgetKey,
+              child: isLogin ? LoginWidget(()=>setState(()=>isLogin=false)) : RegisterWidget(()=>setState(()=>isLogin=true)),
+            ),
           ),
         ],
       )
     );
   }
+
 }
--- lib/page/auth/registerWidget.dart
+++ lib/page/auth/registerWidget.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class RegisterWidget extends StatefulWidget {
-
   final VoidCallback onBackTap;
 
   RegisterWidget(this.onBackTap);
@@ -27,12 +26,12 @@ class _RegisterWidgetState extends State<RegisterWidget> {
   String? _usernameValidator(String? v) {
     if(v!=null) {
       if(v.length<=0) {
-        return '이메일을 입력해주세요.';
+        return '닉네임을 입력해주세요.';
       } else {
         return null;
       }
     } else {
-      return '이메일을 입력해주세요.';
+      return '닉네임을 입력해주세요.';
     }
   }
 
@@ -66,143 +65,145 @@ class _RegisterWidgetState extends State<RegisterWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Align(
-          alignment: Alignment.centerLeft,
-          child: Row(
-            children: [
-              Material(
-                color:Colors.transparent,
-                child: InkWell(
-                  onTap: widget.onBackTap,
-                  borderRadius: BorderRadius.circular(50),
-                  child: Icon(
-                    Icons.arrow_back,
-                    color: Get.theme.primaryColor,
-                  ),
-                ),
-              ),
-              SizedBox(width: 8,),
-              Text(
-                "회원가입",
-                style: TextStyle(
-                  color: Get.theme.primaryColor,
-                  fontWeight: FontWeight.w900,
-                  fontSize: 18
-                ),
-              ),
-            ],
-          )
-        ),
-        Container(
-          margin: EdgeInsets.only(top: 10),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "닉네임",
-                  style: TextStyle(
-                      color: Color(0xb3000000),
-                      fontSize: 12
-                  ),
-                ),
-                TextFormField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Color(0xfff6f6f6),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(50.0),
-                          borderSide: BorderSide.none
+    return Container(
+      child: Column(
+        children: [
+          Align(
+              alignment: Alignment.centerLeft,
+              child: Row(
+                children: [
+                  Material(
+                    color:Colors.transparent,
+                    child: InkWell(
+                      onTap: widget.onBackTap,
+                      borderRadius: BorderRadius.circular(50),
+                      child: Icon(
+                        Icons.arrow_back,
+                        color: Get.theme.primaryColor,
                       ),
                     ),
-                    onSaved: (v)=>_username=v!,
-                    validator: _usernameValidator
-                ),
-                SizedBox(height: 12,),
-                Text(
-                  "이메일 주소",
-                  style: TextStyle(
-                      color: Color(0xb3000000),
-                      fontSize: 12
                   ),
-                ),
-                TextFormField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Color(0xfff6f6f6),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(50.0),
-                          borderSide: BorderSide.none
-                      ),
-                    ),
-                    onSaved: (v)=>_email=v!,
-                    validator: _emailValidator
-                ),
-                SizedBox(height: 12,),
-                Text(
-                  "비밀번호",
-                  style: TextStyle(
-                      color: Color(0xb3000000),
-                      fontSize: 12
-                  ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Color(0xfff6f6f6),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50.0),
-                        borderSide: BorderSide.none
-                    ),
-                  ),
-                  onSaved: (v)=>_password=v!,
-                  validator: _passwordValidator,
-                )
-              ],
-            ),
-          ),
-        ),
-        Container(
-          margin: EdgeInsets.only(top: 30),
-          decoration: BoxDecoration(
-              color: Get.theme.primaryColor,
-              borderRadius: BorderRadius.circular(50),
-              boxShadow: [
-                BoxShadow(
-                    offset: Offset(4,4),
-                    blurRadius: 10,
-                    color: Color(0x40000000)
-                )
-              ]
-          ),
-          child:Material(
-            color: Colors.transparent,
-            child: InkWell(
-              onTap: _register,
-              highlightColor: Color(0x4dffffff),
-              borderRadius: BorderRadius.circular(50),
-              child: Container(
-                width: Get.width,
-                padding: EdgeInsets.all(8),
-                child: Center(
-                  child: Text(
+                  SizedBox(width: 8,),
+                  Text(
                     "회원가입",
                     style: TextStyle(
-                        color: Get.theme.highlightColor,
-                        fontWeight: FontWeight.bold,
+                        color: Get.theme.primaryColor,
+                        fontWeight: FontWeight.w900,
                         fontSize: 18
                     ),
                   ),
+                ],
+              )
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "닉네임",
+                    style: TextStyle(
+                        color: Color(0xb3000000),
+                        fontSize: 12
+                    ),
+                  ),
+                  TextFormField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color(0xfff6f6f6),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50.0),
+                            borderSide: BorderSide.none
+                        ),
+                      ),
+                      onSaved: (v)=>_username=v!,
+                      validator: _usernameValidator
+                  ),
+                  SizedBox(height: 12,),
+                  Text(
+                    "이메일 주소",
+                    style: TextStyle(
+                        color: Color(0xb3000000),
+                        fontSize: 12
+                    ),
+                  ),
+                  TextFormField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color(0xfff6f6f6),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50.0),
+                            borderSide: BorderSide.none
+                        ),
+                      ),
+                      onSaved: (v)=>_email=v!,
+                      validator: _emailValidator
+                  ),
+                  SizedBox(height: 12,),
+                  Text(
+                    "비밀번호",
+                    style: TextStyle(
+                        color: Color(0xb3000000),
+                        fontSize: 12
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xfff6f6f6),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50.0),
+                          borderSide: BorderSide.none
+                      ),
+                    ),
+                    onSaved: (v)=>_password=v!,
+                    validator: _passwordValidator,
+                  )
+                ],
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 30),
+            decoration: BoxDecoration(
+                color: Get.theme.primaryColor,
+                borderRadius: BorderRadius.circular(50),
+                boxShadow: [
+                  BoxShadow(
+                      offset: Offset(4,4),
+                      blurRadius: 10,
+                      color: Color(0x40000000)
+                  )
+                ]
+            ),
+            child:Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: _register,
+                highlightColor: Color(0x4dffffff),
+                borderRadius: BorderRadius.circular(50),
+                child: Container(
+                  width: Get.width,
+                  padding: EdgeInsets.all(8),
+                  child: Center(
+                    child: Text(
+                      "회원가입",
+                      style: TextStyle(
+                          color: Get.theme.highlightColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
