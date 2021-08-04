5bfa9a5aa66005d93b5f6d867dc5e3be093552c4
zeratulspc_zeroxflow <zeratulspc@gmail.com>
2021-08-04 13:42:22 +0900

    refactor User.dart

--- lib/model/User.dart
+++ lib/model/User.dart
@@ -1,18 +1,18 @@
 
 class User {
-  final int userId;
+  final String uid;
   final String username;
   final DateTime registerDate;
 
   User({
-    required this.userId,
+    required this.uid,
     required this.username,
     required this.registerDate,
   });
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
-      userId : json['userId'],
+      uid : json['uid'],
       username : json['username'],
       registerDate : DateTime.parse(json['registerDate'])
     );
@@ -20,7 +20,7 @@ class User {
 
   Map<String,dynamic> toJson() {
     return {
-      'userId' : userId,
+      'uid' : uid,
       'username' : username,
       'registerDate' : registerDate.toIso8601String()
     };
