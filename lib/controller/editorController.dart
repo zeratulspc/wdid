fa7ebb221562a0406255b1844418315c2303c22d
zeratulspc <zeratulspc@gmail.com>
2021-08-21 21:30:53 +0900

    editorController.dart 수정

--- lib/controller/editorController.dart
+++ lib/controller/editorController.dart
@@ -1,7 +1,13 @@
 
 import 'package:get/get.dart';
+import 'package:wdid/data/models/Job.dart';
+import 'package:wdid/data/repository/repository.dart';
 
 class EditorController extends GetxController {
+  final Repository repo;
+
+  EditorController(this.repo);
+
   final _title = "".obs;
   set title(String t) => _title.value = t;
   String get title => _title.value;
@@ -13,4 +19,17 @@ class EditorController extends GetxController {
   final _dateTime = DateTime.now().obs;
   set dateTime(DateTime t) => _dateTime.value = t;
   DateTime get dateTime => _dateTime.value;
+
+  addJob(String uid) async {
+    await repo.addJob(
+      Job(
+        id: '',
+        uid: uid,
+        title: title,
+        body: body,
+        completeDate: dateTime
+      )
+    );
+  }
+
 }
